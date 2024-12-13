ad054802c1e38cb0cf89b2d9b477307953193a7e
Mar4inja <mar4inja19@gmail.com>
2024-12-14 00:29:26 +0100

    update

--- src/main/java/de/ait/chat/config/SecurityConfig.java
+++ src/main/java/de/ait/chat/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5500", "http://localhost:8080", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5500", "http://localhost:8080", "http://localhost:3000", "http://localhost:3001"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // Atļaut cookies
